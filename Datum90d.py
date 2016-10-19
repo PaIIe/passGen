11c697ed0ad54390b567cfcda18edb7953569e7c
Paiie <dannyhofmann93@web.de>
2016-10-19 12:55:39 +0200

    prints the date now n times

--- Datum90d.py
+++ Datum90d.py
@@ -1,6 +1,10 @@
 from datetime import date
 # 19.10.2016 736256days since day 1
-def add90():
-    tempdate = date.fromordinal(736256 + 90)
-    return tempdate
+def add90(n): #prints 19.10.16 +90 Days n times
+    i = 0
+    
+    while(i < n):
+        tempdate = date.fromordinal(736256 + 90 + i)
+        i = i+1
+        print(tempdate)
 
